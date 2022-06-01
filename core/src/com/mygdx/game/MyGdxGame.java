07890069c5ed2310c10bc941e18fef408e565749
DeadLik <mc_grove@bk.ru>
2022-06-01 19:18:16 +0300

    hw2

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -1,31 +1,49 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class MyGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+    SpriteBatch batch;
+    AnimPlayer rhinocerosAnim;
+    Label label;
+    Texture img;
 
-	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    private int x;
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        rhinocerosAnim = new AnimPlayer("NosRogAnim.png", 8, 1, 16, Animation.PlayMode.LOOP);
+        label = new Label(36);
+        img = new Texture("Cloud.jpg");
+    }
+
+    @Override
+    public void render() {
+        ScreenUtils.clear(1, 0, 0, 1);
+
+        if (Gdx.input.isKeyPressed(Input.Keys.A)) x--;
+        if (Gdx.input.isKeyPressed(Input.Keys.D)) x++;
+
+        rhinocerosAnim.step(Gdx.graphics.getDeltaTime());
+
+        batch.begin();
+        batch.draw(img, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        batch.draw(rhinocerosAnim.getFrame(), x, Gdx.graphics.getHeight() / 2);
+        label.draw(batch, "Первая игра!", 0, 0);
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        rhinocerosAnim.dispose();
+        img.dispose();
+    }
 }
