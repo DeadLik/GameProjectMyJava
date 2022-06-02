58d9ae3307d3d47f0f025a417c0faf9b7e5e6253
DeadLik <mc_grove@bk.ru>
2022-06-02 21:03:16 +0300

    hw3

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -3,40 +3,89 @@ package com.mygdx.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.MapLayer;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.MapObjects;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.ScreenUtils;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class MyGdxGame extends ApplicationAdapter {
     SpriteBatch batch;
     AnimPlayer rhinocerosAnim;
     Label label;
-    Texture img;
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer mapRenderer;
+    private OrthographicCamera camera;
+    private List<Coin> coinList;
 
     private int x;
 
     @Override
     public void create() {
+        map = new TmxMapLoader().load("maps/level1.tmx");
+        mapRenderer = new OrthogonalTiledMapRenderer(map);
+
         batch = new SpriteBatch();
         rhinocerosAnim = new AnimPlayer("NosRogAnim.png", 8, 1, 16, Animation.PlayMode.LOOP);
         label = new Label(36);
-        img = new Texture("Cloud.jpg");
+
+        camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        RectangleMapObject o = (RectangleMapObject) map.getLayers().get("Слой объектов 2").getObjects().get("camera");
+        camera.position.x = o.getRectangle().x;
+        camera.position.y = o.getRectangle().y;
+        camera.zoom = 0.5f;
+        camera.update();
+
+        coinList = new ArrayList<>();
+        MapLayer ml = map.getLayers().get("coins");
+        if (ml != null) {
+            MapObjects mo = ml.getObjects();
+            if (mo.getCount() > 0) {
+                for (int i = 0; i < mo.getCount(); i++) {                                       // не придумал как в foreach перевести, перевел нижний for
+                    RectangleMapObject tmpMo = (RectangleMapObject) ml.getObjects().get(i);
+                    Rectangle rect = tmpMo.getRectangle();
+                    coinList.add(new Coin(new Vector2(rect.x, rect.y)));
+                }
+            }
+        }
     }
 
     @Override
     public void render() {
-        ScreenUtils.clear(1, 0, 0, 1);
+        ScreenUtils.clear(Color.CYAN);
 
-        if (Gdx.input.isKeyPressed(Input.Keys.A)) x--;
-        if (Gdx.input.isKeyPressed(Input.Keys.D)) x++;
+        if (Gdx.input.isKeyPressed(Input.Keys.A)) camera.position.x--;
+        if (Gdx.input.isKeyPressed(Input.Keys.D)) camera.position.x++;
+        if (Gdx.input.isKeyPressed(Input.Keys.W)) camera.position.y++;
+        if (Gdx.input.isKeyPressed(Input.Keys.S)) camera.position.y--;
+        camera.update();
+
+        mapRenderer.setView(camera);
+        mapRenderer.render();
 
         rhinocerosAnim.step(Gdx.graphics.getDeltaTime());
 
         batch.begin();
-        batch.draw(img, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        batch.draw(rhinocerosAnim.getFrame(), x, Gdx.graphics.getHeight() / 2);
+        batch.draw(rhinocerosAnim.getFrame(), Gdx.graphics.getHeight() / 2, Gdx.graphics.getHeight() / 2);
         label.draw(batch, "Первая игра!", 0, 0);
+
+        for (Coin coin : coinList) {
+            coin.draw(batch, camera);
+        }
+
         batch.end();
     }
 
@@ -44,6 +93,6 @@ public class MyGdxGame extends ApplicationAdapter {
     public void dispose() {
         batch.dispose();
         rhinocerosAnim.dispose();
-        img.dispose();
+        coinList.get(0).dispose();
     }
 }
