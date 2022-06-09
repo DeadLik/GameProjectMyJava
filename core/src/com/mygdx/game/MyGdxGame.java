d3a4fcfdd01453b6c09f063d2b1735a5da99fab2
DeadLik <mc_grove@bk.ru>
2022-06-09 23:23:30 +0300

    hw4

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -5,8 +5,10 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.maps.MapLayer;
 import com.badlogic.gdx.maps.MapObject;
 import com.badlogic.gdx.maps.MapObjects;
@@ -23,23 +25,36 @@ import java.util.Iterator;
 import java.util.List;
 
 public class MyGdxGame extends ApplicationAdapter {
-    SpriteBatch batch;
-    AnimPlayer rhinocerosAnim;
-    Label label;
+    private SpriteBatch batch;
+    //private ShapeRenderer renderer; //ДЛЯ ОТЛАДКИ
+    private Label label;
     private TiledMap map;
     private OrthogonalTiledMapRenderer mapRenderer;
     private OrthographicCamera camera;
     private List<Coin> coinList;
+    private Texture fon;
+    private MyCharacter nosRog;
 
-    private int x;
+    private int[] foreGround, backGround;
+
+    private int score;
 
     @Override
     public void create() {
+        nosRog = new MyCharacter();
+        fon = new Texture("fon.png");
         map = new TmxMapLoader().load("maps/level1.tmx");
         mapRenderer = new OrthogonalTiledMapRenderer(map);
 
+        foreGround = new int[1];
+        foreGround[0] = map.getLayers().getIndex("Слой тайлов 2");
+        backGround = new int[1];
+        backGround[0] = map.getLayers().getIndex("Слой тайлов 1");
+
         batch = new SpriteBatch();
-        rhinocerosAnim = new AnimPlayer("NosRogAnim.png", 8, 1, 16, Animation.PlayMode.LOOP);
+        //renderer = new ShapeRenderer(); //ДЛЯ ОТЛАДКИ
+
+
         label = new Label(36);
 
         camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
@@ -54,7 +69,7 @@ public class MyGdxGame extends ApplicationAdapter {
         if (ml != null) {
             MapObjects mo = ml.getObjects();
             if (mo.getCount() > 0) {
-                for (int i = 0; i < mo.getCount(); i++) {                                       // не придумал как в foreach перевести, перевел нижний for
+                for (int i = 0; i < mo.getCount(); i++) {
                     RectangleMapObject tmpMo = (RectangleMapObject) ml.getObjects().get(i);
                     Rectangle rect = tmpMo.getRectangle();
                     coinList.add(new Coin(new Vector2(rect.x, rect.y)));
@@ -65,34 +80,66 @@ public class MyGdxGame extends ApplicationAdapter {
 
     @Override
     public void render() {
-        ScreenUtils.clear(Color.CYAN);
+        ScreenUtils.clear(1, 0, 0, 1);
 
-        if (Gdx.input.isKeyPressed(Input.Keys.A)) camera.position.x--;
-        if (Gdx.input.isKeyPressed(Input.Keys.D)) camera.position.x++;
+        nosRog.setWalk(false);
+        if (Gdx.input.isKeyPressed(Input.Keys.A)) {
+            camera.position.x--;
+            nosRog.setDir(true);
+            nosRog.setWalk(true);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.D)) {
+            camera.position.x++;
+            nosRog.setDir(false);
+            nosRog.setWalk(true);
+        }
+
         if (Gdx.input.isKeyPressed(Input.Keys.W)) camera.position.y++;
         if (Gdx.input.isKeyPressed(Input.Keys.S)) camera.position.y--;
+
         camera.update();
 
-        mapRenderer.setView(camera);
-        mapRenderer.render();
+        batch.begin();
+        batch.draw(fon, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        batch.end();
 
-        rhinocerosAnim.step(Gdx.graphics.getDeltaTime());
+        mapRenderer.setView(camera);
+        mapRenderer.render(backGround);
+        mapRenderer.render(foreGround);
 
         batch.begin();
-        batch.draw(rhinocerosAnim.getFrame(), Gdx.graphics.getHeight() / 2, Gdx.graphics.getHeight() / 2);
-        label.draw(batch, "Первая игра!", 0, 0);
+        batch.draw(nosRog.getFrame(), Gdx.graphics.getHeight() / 2, Gdx.graphics.getHeight() / 2);
+        label.draw(batch, "Монет собрано: " + String.valueOf(score), 0, 0);
 
-        for (Coin coin : coinList) {
-            coin.draw(batch, camera);
+        for (int i = 0; i < coinList.size(); i++) {
+            coinList.get(i).draw(batch, camera);
+            if (coinList.get(i).isOverlaps(nosRog.getRect(), camera)) {
+                coinList.remove(i);
+                score++;
+            }
         }
 
         batch.end();
+
+/*        Color heroClr = new Color(Color.WHITE);
+        mapRenderer.render(foreGround);
+        renderer.setColor(heroClr);
+        renderer.begin(ShapeRenderer.ShapeType.Line);
+        for (int i = 0; i < coinList.size(); i++) {
+            coinList.get(i).shapeDraw(renderer, camera);
+            if (coinList.get(i).isOverlaps(nosRog.getRect(), camera)) {
+                coinList.remove(i);
+                heroClr = Color.BLUE;
+            }
+        }
+        renderer.setColor(heroClr);
+        renderer.rect(nosRog.getRect().x, nosRog.getRect().y, nosRog.getRect().width, nosRog.getRect().height);
+        renderer.end();*/ // ДЛЯ ОТЛАДКИ
     }
 
     @Override
     public void dispose() {
         batch.dispose();
-        rhinocerosAnim.dispose();
         coinList.get(0).dispose();
     }
 }
